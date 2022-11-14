47d5e5520347323b21457f9edc8d5a8083a46da9
rama-adi <17980909+rama-adi@users.noreply.github.com>
2022-11-14 23:46:01 +0700

    wip

--- utils/HttpResponse.go
+++ utils/HttpResponse.go
@@ -1,8 +1,8 @@
 package utils
 
 type HttpResponse[T any] struct {
-	Success    bool
-	Message    string
-	StatusCode int
-	Body       T
+	Success    bool   `json:"success"`
+	Message    string `json:"message"`
+	StatusCode int    `json:"status_code"`
+	Body       T      `json:"body"`
 }
